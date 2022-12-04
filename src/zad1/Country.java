e34e56012d4cabcda16aa73022c93c3f19882f73
jbzurek <kubazurek1601@gmail.com>
2022-12-04 16:55:10 +0100

    Initial commit

---
+++ src/zad1/Country.java
@@ -0,0 +1,32 @@
+package zad1;
+
+public class Country {
+
+    private final String flag;
+    private final String country;
+    private final String capital;
+    private final int population;
+
+    public Country(String country, String capital, int population) {
+        this.flag = "BaH.png";
+        this.country = country;
+        this.capital = capital;
+        this.population = population;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+}
---
+++ src/zad1/CountryTable.java
@@ -0,0 +1,65 @@
+package zad1;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountryTable {
+
+    private final String fileName;
+    private final List<Country> countryList = new ArrayList<>();
+    private String[] columnsName;
+
+    public CountryTable(String countriesFileName) {
+        this.fileName = countriesFileName;
+    }
+
+    public JTable create() throws IOException {
+        load();
+        return new JTable(createRows(countryList), columnsName);
+    }
+
+    private void load() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+        columnsName = reader.readLine().split("\t");
+        String line;
+        do {
+            line = reader.readLine();
+            if (line != null) {
+                String[] countryData = line.split("\t");
+                countryList.add(new Country(countryData[1], countryData[2], Integer.parseInt(countryData[3])));
+            }
+        } while (line != null);
+    }
+
+    DefaultTableModel model = new DefaultTableModel() {
+        @Override
+        public Class<?> getColumnClass(int columnIndex) {
+            return super.getColumnClass(columnIndex);
+        }
+    };
+
+    private Object[][] createRows (List<Country> countryList) {
+        int listLength = countryList.size();
+        Object[][] rows = new Object[listLength][4];
+        for (int i = 0; i < listLength; i++) {
+            Country country = countryList.get(i);
+            rows[i][0] = new ImageIcon(country.getFlag());
+            rows[i][1] = country.getCountry();
+            rows[i][2] = country.getCapital();
+            rows[i][3] = country.getPopulation();
+        }
+        return rows;
+    }
+
+
+
+
+
+
+}
